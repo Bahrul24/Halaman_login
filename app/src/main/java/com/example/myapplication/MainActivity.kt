93c62d758bd62356a56dfea47d04624e4d6b0467
Bahrul24 <dimasbahrulikhwani851@gmail.com>
2025-02-26 00:00:58 +0700

    Menyesuaikan Main dan login activity beserta XML

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.myapplication
 
+import com.example.myapplication.databinding.ActivityMainBinding
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Get Email from LoginActivity
+        val userEmail = intent.getStringExtra("USER_EMAIL")
+        binding.tvUserEmail.text = "Logged in as: $userEmail"
+
+        // Log email to LogCat
+        Log.d("MainActivity", "User Email: $userEmail")
         }
-    }
 }
